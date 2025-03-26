b89572f7385800b6687b0256baa729c8bc5bbe2a
AlexG <alexgreau@live.fr>
2025-03-26 22:42:34 +0800

    feat: added footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
-
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped lang="scss">
@@ -11,7 +11,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
-
-
+import Footer from './components/Footer.vue';
 
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/projects">Projects</RouterLink>
+        </nav>
+    </footer>
+</template>
+<style lang="scss" scoped>
+footer {
+    background: pink;
+}
+</style>
