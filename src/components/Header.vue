f60972cfa37a4774e25c0136e9a861823b7ce5d9
AlexG <alexgreau@live.fr>
2025-03-26 22:35:43 +0800

    chore: cleanup of unused files + added 404

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/projects">Projects</RouterLink>
+        </nav>
+    </header>
+</template>
+<style lang="sass" scoped>
+    
+
+</style>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<template>
+    
+<section>
+    Oops, looks like you swam too far
+    <RouterLink to="/">Go back to safety</RouterLink>
+</section>            
+</template>
+<style lang="sass" scoped>
+
+</style>
