a815e8fa68a2322e1fccbdd0a1cee9d801dbcdd7
AlexG <alexgreau@live.fr>
2025-04-07 21:33:23 +0800

    feat: Coins clickable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,24 +3,30 @@
     <section class="hero-wrapper">
       <div>
         <h1>My Portfolio</h1>
-        <p>Welcome to my portfolio! I'm a passionate developer.<br/>
-        Here you can find my projects, skills, and more about me.<br/>
-        Feel free to explore and reach out if you have any questions!</p>
+        <p>
+          Welcome to my portfolio! I'm a passionate developer.<br />
+          Here you can find my projects, skills, and more about me.<br />
+          Feel free to explore and reach out if you have any questions!
+        </p>
         <ul>
-          <li><a href="https://drive.google.com/file/d/1DEd9sTz8nBU5GiNecM1OBAAv88Kr_LZM/view?usp=drive_link" target="_blank">My CV</a></li>
           <li>
-            <a href="mailto:alexgreau@live.fr?subject=Job%20Prospect">
-              Hire me
-            </a>
+            <a
+              href="https://drive.google.com/file/d/1DEd9sTz8nBU5GiNecM1OBAAv88Kr_LZM/view?usp=drive_link"
+              target="_blank"
+              >My CV</a
+            >
+          </li>
+          <li>
+            <a href="mailto:alexgreau@live.fr?subject=Job%20Prospect"> Hire me </a>
           </li>
           <li>
             <a href="https://github.com/AlexGreau" target="_blank">
-              <img src="../assets/icons/github.svg" alt="GitHub"/>
+              <img src="../assets/icons/github.svg" alt="GitHub" />
             </a>
           </li>
           <li>
             <a href="https://www.linkedin.com/in/alexandre-greau/" target="_blank">
-              <img src="../assets/icons/linkedin.svg" alt="LinkedIn"/>
+              <img src="../assets/icons/linkedin.svg" alt="LinkedIn" />
             </a>
           </li>
           <li></li>
@@ -29,129 +35,114 @@
       <figure>
         <img src="../assets/img/me.jpg" alt="Alex Greau" />
         <figcaption>This website's creator</figcaption>
-      </figure>    
+      </figure>
     </section>
     <section class="project-finder">
       <h2>What i do</h2>
-      <p>I'm a developer with a passion for creating web applications. I love working with modern technologies and frameworks to build efficient and user-friendly solutions.</p>
-      <p>My main focus is on front-end development, but I also have experience in back-end technologies. I'm always eager to learn new skills and improve my craft.</p>
-      
+      <p>
+        I'm a developer with a passion for creating web applications. I love working with modern
+        technologies and frameworks to build efficient and user-friendly solutions.
+      </p>
+      <p>
+        My main focus is on front-end development, but I also have experience in back-end
+        technologies. I'm always eager to learn new skills and improve my craft.
+      </p>
       <form>
-        List of checkboxes as clickable icons with text, round and glassy. Which would filter projects.
-        <ul>
-          <li>
-            <label>
-              <input type="checkbox" name="vue" value="Vue">
-              <img src="@/assets/icons/vuejs.svg" alt="Vue.js" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="react" value="React">
-              <img src="@/assets/icons/react.svg" alt="React" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="nodejs" value="Node.js">
-              <img src="@/assets/icons/nodejs.svg" alt="Node.js" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="typescript" value="TypeScript">
-              <img src="@/assets/icons/typescript.svg" alt="TypeScript" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="html" value="HTML">
-              <img src="@/assets/icons/html.svg" alt="HTML" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" id="js" name="js" value="JavaScript">
-              <img src="@/assets/icons/javascript.svg" alt="JavaScript" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="figma" value="Figma">
-              <img src="@/assets/icons/figma.svg" alt="Figma" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="scss" value="SCSS">
-              <img src="@/assets/icons/sass.svg" alt="SCSS" />
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="express" value="Express">
-              Express
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="AWS" value="AWS">
-              <img src="@/assets/icons/aws.svg" alt="aws"/>
-            </label>
-          </li>
-          <!-- Add more checkboxes as needed -->
-        </ul>
+        List of checkboxes as clickable icons with text, round and glassy. Which would filter
+        projects.
+        <fieldset>
+          <legend>Filter by techs</legend>
+          <label>
+            <input type="checkbox" name="vue" value="Vue" />
+            <img src="@/assets/icons/vuejs.svg" alt="Vue.js" />
+          </label>
+          <label>
+            <input type="checkbox" name="react" value="React" />
+            <img src="@/assets/icons/react.svg" alt="React" />
+          </label>
+          <label>
+            <input type="checkbox" name="nodejs" value="Node.js" />
+            <img src="@/assets/icons/nodejs.svg" alt="Node.js" />
+          </label>
+          <label>
+            <input type="checkbox" name="typescript" value="TypeScript" />
+            <img src="@/assets/icons/typescript.svg" alt="TypeScript" />
+          </label>
+          <label>
+            <input type="checkbox" name="html" value="HTML" />
+            <img src="@/assets/icons/html.svg" alt="HTML" />
+          </label>
+          <label>
+            <input type="checkbox" id="js" name="js" value="JavaScript" />
+            <img src="@/assets/icons/javascript.svg" alt="JavaScript" />
+          </label>
+          <label>
+            <input type="checkbox" name="figma" value="Figma" />
+            <img src="@/assets/icons/figma.svg" alt="Figma" />
+          </label>
+          <label>
+            <input type="checkbox" name="scss" value="SCSS" />
+            <img src="@/assets/icons/sass.svg" alt="SCSS" />
+          </label>
+          <label>
+            <input type="checkbox" name="express" value="Express" />
+            Express
+          </label>
+          <label>
+            <input type="checkbox" name="AWS" value="AWS" />
+            <img src="@/assets/icons/aws.svg" alt="aws" />
+          </label>
+        </fieldset>
+        <!-- Add more checkboxes as needed -->
       </form>
     </section>
     <section>
-      Project list as glassy cards with an image and a title + badges of techs + small intro
-      need a neat hover effect
-      <br/>
-      <ProjectCard
-        v-for="project in projects"
-        :key="project.id"
-        :project="project"
-      />
+      Project list as glassy cards with an image and a title + badges of techs + small intro need a
+      neat hover effect
+      <br />
+      <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
     </section>
   </main>
 </template>
 <script setup lang="ts">
-import ProjectCard from '@/components/cards/ProjectCard.vue';
-import { ref } from 'vue';
-import type { Project } from '@/model/Project';
+import ProjectCard from '@/components/cards/ProjectCard.vue'
+import { ref } from 'vue'
+import type { Project } from '@/model/Project'
 
 const projects = ref<Array<Project>>([
   {
     id: 1,
-    name: "Portfolio Website",
-    techStack: ["Vue", "Scss", "HTML/CSS", "TypeScript"],
-    status: "Completed",
-    description: "A personal portfolio website to showcase my projects and skills.",
-    image: "../assets/img/portfolio.png",
-    route: "/projects/portfolio",
+    name: 'Portfolio Website',
+    techStack: ['Vue', 'Scss', 'HTML/CSS', 'TypeScript'],
+    status: 'Completed',
+    description: 'A personal portfolio website to showcase my projects and skills.',
+    image: '../assets/img/portfolio.png',
+    route: '/projects/portfolio',
   },
   {
     id: 2,
-    name: "Workout timer",
-    techStack: ["React", "Node.js", "Figma"],
-    status: "In Progress",
-    description: "A workout helper to plan and execute your workouts.",
-    image: "../assets/img/task-manager.png",
-    route: "/projects/workout-timer",
+    name: 'Workout timer',
+    techStack: ['React', 'Node.js', 'Figma'],
+    status: 'In Progress',
+    description: 'A workout helper to plan and execute your workouts.',
+    image: '../assets/img/task-manager.png',
+    route: '/projects/workout-timer',
   },
   {
     id: 3,
-    name: "Swim coach website",
-    techStack: ["Vue", "Node.js", "MongoDB"],
-    status: "To do",
-    description: "My swim coaching services and how to reach out to me.",
-    image: "../assets/img/task-manager.png",
-    route: "/projects/swim-coach",
+    name: 'Swim coach website',
+    techStack: ['Vue', 'Node.js', 'MongoDB'],
+    status: 'To do',
+    description: 'My swim coaching services and how to reach out to me.',
+    image: '../assets/img/task-manager.png',
+    route: '/projects/swim-coach',
   },
-]);
+])
 </script>
 
 <style lang="scss">
+@use '@/assets/common/variables.scss' as *; // Import variables
+@use '@/assets/common/theme.scss' as *; // Import theme styles and variables
 .main-wrapper {
   display: flex;
   flex-direction: column;
@@ -195,31 +186,39 @@ const projects = ref<Array<Project>>([
 
   & > section.project-finder {
     form {
-      ul {
+      fieldset {
         display: flex;
         gap: 10px;
         list-style-type: none;
-        li {
-          input[type="checkbox"] {
-            display: none; // Hide the default checkbox
-          }
-          label {
-            display: inline-block;
-            padding: 10px 20px;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        legend {
+          display: none;
+        }
+        label {
+          img {
             border-radius: 50%;
-            background-color: rgba(255, 255, 255, 0.8);
-            cursor: pointer;
-            transition: background-color 0.3s ease-in-out;
+            width: 48px;
+            height: 48px;
+            padding: $spacing-md;
+            display: inline-block;
+            transition: all 0.3s ease;
+            &:focus,
             &:hover {
-              background-color: pink;
+              background-color: aquamarine;
             }
+          }
+          input[type='checkbox']:checked + img {
+            background-color: pink;
+          }
+
+          input[type='checkbox'] {
+            display: none; // Hide the default checkbox
           }
         }
       }
     }
   }
 }
-
-
 </style>
-
