8d98f85d94811521b393b27fdd41077b9f5ceb22
AlexG <alexgreau@live.fr>
2025-04-19 22:40:49 +0800

    feat: halo cursor

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
- <!-- <Header /> --> 
+ <!-- <Header /> -->
+  <HaloCursor />
   <RouterView id="app-wrapper"/>
   <!-- <Footer /> -->
 </template>
@@ -25,5 +26,6 @@ import { RouterView } from 'vue-router';
 import Footer from './components/layout/Footer.vue';
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
+import HaloCursor from './components/common/HaloCursor.vue';
 
 </script>
