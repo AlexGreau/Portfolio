d02596a28734147ff724d8d529e3cdb4bf8018ab
AlexG <alexgreau@live.fr>
2025-04-22 22:55:04 +0800

    feat: new project card + refactor

--- src/components/cards/ProjectCard.vue
+++ src/components/cards/ProjectCard.vue
@@ -1,24 +1,23 @@
 <template>
-    <article class="project-card">
-        <h4>{{ project.name }}</h4>
-        <p>{{ project.description }}</p>
-        <ul>
-            <li v-for="(tech, index) in project.techStack" :key="index" class="pill">{{ tech }}</li>
-        </ul>
-        <router-link v-if="project.route" :to="project.route" class="button">See more</router-link>
-    </article>
+  <article class="card project">
+    <router-link v-if="project.route" :to="project.route">{{ project.name }}</router-link>
+    <p>{{ project.description }}</p>
+    <ul class="tech-stack">
+      <li v-for="(tech, index) in project.techStack" :key="index" class="pill">{{ tech }}</li>
+    </ul>
+  </article>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
-import type { Project } from '../../model/Project';
-import { RouterLink } from 'vue-router';
+import { defineComponent } from 'vue'
+import type { Project } from '../../model/Project'
+import { RouterLink } from 'vue-router'
 export default defineComponent({
-    name: 'ProjectCard',
-    props: {
-        project: {
-            type: Object as () => Project,
-            required: true,
-        },
+  name: 'ProjectCard',
+  props: {
+    project: {
+      type: Object as () => Project,
+      required: true,
     },
-});
+  },
+})
 </script>
--- src/components/cards/WorkCard.vue
+++ src/components/cards/WorkCard.vue
@@ -1,16 +1,16 @@
 <template>
-  <article class="work-card">
+  <article class="card work">
     <div>
       <a :href="work.link" target="_blank">{{ `${work.position} - ${work.company}` }}</a>
       <span class="date">{{ formatDoubleDates(work.startDate, work.endDate) }}</span>
     </div>
-      <p>{{ work.description }}</p>
-      <ul>
-        <li v-for="(item, index) in work.tasks" :key="index">{{ item }}</li>
-      </ul>
-      <ul class="tech-stack">
-        <li v-for="(tech, index) in work.techs" :key="index" class="pill">{{ tech }}</li>
-      </ul>
+    <p>{{ work.description }}</p>
+    <ul>
+      <li v-for="(item, index) in work.tasks" :key="index">{{ item }}</li>
+    </ul>
+    <ul class="tech-stack">
+      <li v-for="(tech, index) in work.techs" :key="index" class="pill">{{ tech }}</li>
+    </ul>
   </article>
 </template>
 <script lang="ts">
@@ -33,13 +33,13 @@ export default defineComponent({
     formatDate(date: Date): string {
       return `${this.months[date.getMonth()]} ${String(date.getFullYear())}`
     },
-    formatDoubleDates(startDate: Date, endDate: Date|undefined): string {
+    formatDoubleDates(startDate: Date, endDate: Date | undefined): string {
       if (!startDate) {
         return ''
       } else if (!endDate) {
         return `${this.formatDate(new Date(startDate))} - Present`
       }
-        return `${this.formatDate(new Date(startDate))} - ${this.formatDate(new Date(endDate))}`
+      return `${this.formatDate(new Date(startDate))} - ${this.formatDate(new Date(endDate))}`
     },
   },
 })
