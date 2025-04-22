7de1f6ddcbbd55bc9ca2590ef250869d9afcf90f
AlexG <alexgreau@live.fr>
2025-04-22 21:27:20 +0800

    infra: changing to docs

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: 'docs', // Change the output directory to 'docs'
+  },
   css: {
     preprocessorOptions: {
       scss: {
