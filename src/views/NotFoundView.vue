f759b4a2810cf14d11fc1716084421ffe82fd0ae
AlexG <alexgreau@live.fr>
2025-04-24 23:18:54 +0800

    feat: icon + style

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,40 +1,38 @@
 <template>
-    <section>
-        <div>
-            A nice image to illustrate the not found
-        </div>
-        Oops, looks like you swam too far
-        <RouterLink to="/">Go back to safety</RouterLink>
-    </section>            
+  <section>
+    <img src="@/assets/img/buoy.png" alt="Buoy" width="200" height="200" />
+    Oops, looks like you swam too far
+    <RouterLink to="/">Go back to safety</RouterLink>
+  </section>
 </template>
 <style lang="scss" scoped>
-    section {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        min-height: 100vh;
-        padding: 20px;
-        text-align: center;
-        font-size: 1.5rem;
-        color: #333;
-    }
-    a {
-        margin-top: 20px;
-        text-decoration: none;
-        color: #007BFF;
-    }
-    a:hover {
-        text-decoration: underline;
-    }
-
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 20px;
+  text-align: center;
+  font-size: 1.5rem;
+  gap: 1rem;
+}
+img {
+  width: 200px;
+  height: 200px;
+}
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
 </style>
 <script lang="ts" setup>
 // onm mount scroll to top
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 
 onMounted(() => {
-    window.scrollTo(0, 0);
-});
-
+  window.scrollTo(0, 0)
+})
 </script>
